a08c5d17d32aa719fea0d44d485a5d2fec7f972b
Qwerty-Space <oliver_duce@live.co.uk>
2020-02-28 14:34:27 +0000

    Removed usless print from start plugin

--- plugins/start_plugin.py
+++ plugins/start_plugin.py
@@ -10,7 +10,6 @@ from .global_functions import log
 # /start
 @events.register(events.NewMessage(pattern=r"/start$"))
 async def on_start(event):
-    print((await event.client.get_me()).first_name)
     if event.is_private:    # If command was sent in private
         await log(event)    # Logs the event
         await event.respond('This is a bot for kicking deleted accounts from groups.  See /help for more info.')
