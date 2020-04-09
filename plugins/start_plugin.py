7e783e9d0ca4cd56ed8e6e931d789748121c1162
Qwerty-Space <oliver_duce@live.co.uk>
2020-04-09 23:57:26 +0100

    improve formatting

--- plugins/start_plugin.py
+++ plugins/start_plugin.py
@@ -14,9 +14,9 @@ async def on_start(event):
     if event.is_private:    # If command was sent in private
         await log(event)    # Logs the event
         await event.respond(
-            "This is a bot for kicking deleted accounts from groups.  " +
-            "It requires the `ban user` permission in groups, any permission in channels.  " +
-            "See /help for more info."
+            "This is a bot for kicking deleted accounts from groups.  "
+            + "It requires the `ban user` permission in groups, any permission in channels.  "
+            + "See /help for more info."
 )
 
 
